ae8acaa9b88afc30fc35fb0ed51444c107b8a009
Saad El Maaqili <sel-maaq@c2r5p12.1337.ma>
2024-12-15 19:12:39 +0100

    starting

---
+++ push_swap/main.c
@@ -0,0 +1,12 @@
+
+#include "push_swap.h"
+
+int main(int argc, char **argv)
+{
+
+    
+    if (argc <= 2)
+    {
+
+    }
+}
---
+++ push_swap/push_swap.h
@@ -0,0 +1,10 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stddef.h>
+
+
+#endif
