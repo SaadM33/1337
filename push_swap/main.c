a0c58aaf50d0abca33f29aefbe439ce7bcf7abfc
SaadM33 <sel-maaq@c3r6p2.1337.ma>
2024-12-19 12:30:40 +0100

    i love ni-

--- push_swap/main.c
+++ push_swap/main.c
@@ -1,18 +1,19 @@
 
 #include "push_swap.h"
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
 	int i;
 	int temp;
 
-	i = 0;
-	if (argc <= 2)
+	i = 1;
+	if (argc >= 2)
 	{
 		while(argv[i])
 		{
 			temp = ft_atoi(argv[i]);
-			
+			printf("%d\n", temp);
 			i++;
 		}
 	}
