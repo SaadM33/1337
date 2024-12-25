db27454bd44dc8a9309ea694554b446bf652d085
SaadM33 <smaaqili@gmail.com>
2024-12-25 10:10:22 +0100

    made operations and .h

--- push_swap/main.c
+++ push_swap/main.c
@@ -1,7 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-maaq <sel-maaq@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/24 21:48:21 by sel-maaq          #+#    #+#             */
+/*   Updated: 2024/12/24 21:48:24 by sel-maaq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
-
 
 int	main(int argc, char **argv)
 {
@@ -17,13 +26,10 @@ int	main(int argc, char **argv)
 	return (0);
 }
 
-
-
 void handle_input(char **av, int ac, t_stack **stack_a)
 {
     int     i;
     char    **args;
-    int     tmp;
 
     i = 0;
     if (ac == 2)
@@ -34,7 +40,7 @@ void handle_input(char **av, int ac, t_stack **stack_a)
     {
         if (!is_valid_number(args[i]))
 			handle_error(stack_a, args, ac);
-		if (!check_duplicate(*stack_a, ft_atol(args[i])));
+		else if (!check_duplicate(*stack_a, ft_atol(args[i])))
 			handle_error(stack_a, args, ac);
 		add_to_stack(stack_a, ft_atol(args[i]), args, ac);
         i++;
@@ -42,38 +48,6 @@ void handle_input(char **av, int ac, t_stack **stack_a)
     if (ac == 2)
         free_split(args);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int check_duplicate(t_stack *stack, int value)
-{
-    while (stack)
-    {
-        if (stack->value == value)
-            return (0);
-        stack = stack->next;
-    }
-    return (1);
-}
-
 
 void add_to_stack(t_stack **stack, int value, char **args, int ac)
 {
@@ -96,32 +70,6 @@ void add_to_stack(t_stack **stack, int value, char **args, int ac)
     }
 }
 
-
-
-int is_valid_number(char *str)
-{
-    int     i;
-    long    num;
-
-    i = 0;
-    if (str[i] == '-' || str[i] == '+')
-        i++;
-    if (!str[i])
-        return (0);
-    while (str[i])
-    {
-        if (!ft_isdigit(str[i]))
-            return (0);
-        i++;
-    }
-    num = ft_atol(str);
-    if (num > INT_MAX || num < INT_MIN)
-		return (0);
-    return (1);
-}
-
-
-
 void handle_error(t_stack **stack_a, char **args, int ac)
 {
     free_stack(stack_a);
@@ -129,58 +77,4 @@ void handle_error(t_stack **stack_a, char **args, int ac)
 		free_split(args);
     write(2, "Error\n", 6);
     exit(EXIT_FAILURE);
-}
-
-
-
-
-void	free_split(char **args)
-{
-	int	i = 0;
-
-	while (args[i])
-	{
-		free(args[i]);
-		i++;
-	}
-	free(args);
-}
-
-
-
-void free_stack(t_stack **stack)
-{
-    t_stack *temp;
-
-    while (*stack)
-    {
-        temp = (*stack)->next;
-        free(*stack);
-        *stack = temp;
-    }
-}
-long ft_atol(const char *str)
-{
-    int     i;
-    int     sign;
-    long    result;
-
-    i = 0;
-    sign = 1;
-    result = 0;
-
-    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-        i++;
-    if (str[i] == '-' || str[i] == '+')
-    {
-        if (str[i] == '-')
-            sign = -1;
-        i++;
-    }
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        result = result * 10 + (str[i] - '0');
-        i++;
-    }
-    return (result * sign);
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-maaq <sel-maaq@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/24 22:29:20 by sel-maaq          #+#    #+#             */
+/*   Updated: 2024/12/25 09:57:29 by sel-maaq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
@@ -17,9 +29,26 @@ typedef struct s_stack
 void	handle_input(char **av, int ac, t_stack **stack_a);
 int		is_valid_number(char *str);
 void	add_to_stack(t_stack **stack, int value, char **args, int ac);
-void	handle_error(t_stack **stack, char **args);
+void 	handle_error(t_stack **stack_a, char **args, int ac);
+void 	add_to_stack(t_stack **stack, int value, char **args, int ac);
 void	free_split(char **args);
+void 	free_stack(t_stack **stack);
 long	ft_atol(const char *str);
+int 	check_duplicate(t_stack *stack, int value);
 
+void    push_a(t_stack **stack_a, t_stack **stack_b);
+void    push_b(t_stack **stack_a, t_stack **stack_b);
+
+void    rot_a(t_stack **stack_a);
+void    rot_b(t_stack **stack_b);
+void    rr(t_stack **stack_a, t_stack **stack_b);
+
+void    swap_a(t_stack **stack_a);
+void    swap_b(t_stack **stack_b);
+void    ss(t_stack **stack_a, t_stack **stack_b);
+
+void    revrot_a(t_stack **stack_a);
+void    revrot_b(t_stack **stack_b);
+void    rrr(t_stack **stack_a, t_stack **stack_b);
 
 #endif
