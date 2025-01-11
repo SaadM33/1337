02e552c7cf9ce83820fb8ee86c5a0b22423137f5
SaadM33 <smaaqili@gmail.com>
2025-01-11 18:27:14 +0100

    done map parsing and moded makefile

--- so_long/map_parser.c
+++ so_long/map_parser.c
@@ -1,31 +1,23 @@
 #include "so_long.h"
 
-static int count_map_lines(char *filename)
-{
-	int		fd;
-	int		lines;
-	char	*line;
-
-	lines = 0;
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (-1);
-	while (get_next_line(fd) > 0)
-	{
-		lines++;
-		free(line);
-	}
-	free(line);
-	close(fd);
-	return (lines);
-}
-
 static int init_map(t_map *map, char *filename)
 {
-	int height;
+	int		height;
+	int		fd;
+	char	*line;
 
-	height = count_map_lines(filename);
-	if (height <= 0)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	height = 0;
+	line = get_next_line(fd);
+	while (line)
+	{
+		height++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	if (height == 0)
 		return (0);
 	map->height = height;
 	map->grid = malloc(sizeof(char *) * (height + 1));
@@ -34,86 +26,80 @@ static int init_map(t_map *map, char *filename)
 	map->collectibles = 0;
 	map->exit = 0;
 	map->player = 0;
-	return (1);
-}
-
-static int validate_chars(t_map *map)
-{
-	int i;
-	int j;
-
-	i = -1;
-	while (++i < map->height)
-	{
-		j = -1;
-		while (map->grid[i][++j])
-		{
-			if (map->grid[i][j] == 'P')
-			{
-				map->player++;
-				map->player_x = j;
-				map->player_y = i;
-			}
-			else if (map->grid[i][j] == 'E')
-				map->exit++;
-			else if (map->grid[i][j] == 'C')
-				map->collectibles++;
-			else if (map->grid[i][j] != '0' && map->grid[i][j] != '1')
-				return (0);
-		}
-	}
-	return (map->player == 1 && map->exit == 1 && map->collectibles > 0);
+	return (close(fd), 1);
 }
 
 int parse_map(t_map *map, char *filename)
 {
 	int     fd;
-	int     i;
+	int		i;
 	char    *line;
 
 	if (!init_map(map, filename))
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)	
+	if (fd < 0)
 		return (0);
 	i = 0;
-	map->width = 0;
 	line = get_next_line(fd);
 	while (line)
-	{		
-		map->grid[i] = line;
+	{
+		if (line[ft_strlen(line) - 1] == '\n')
+    		line[ft_strlen(line) - 1] = '\0';
+		map->grid[i++] = line;
 		if (i == 0)
 			map->width = ft_strlen(line);
-		else if ((int)ft_strlen(line) != map->width)
-			return (0);
 		line = get_next_line(fd);
-		i++;
 	}
 	map->grid[i] = NULL;
 	close(fd);
-	return (validate_chars(map));
-}
-
-int validate_map(t_map *map)
-{
-	int i;
-	int j;
-
-	// Check if map is surrounded by walls
-	i = -1;
-	while (++i < map->height)
-	{
-		j = -1;
-		while (++j < map->width)
-		{
-			if (i == 0 || i == map->height - 1 || 
-				j == 0 || j == map->width - 1)
-			{
-				if (map->grid[i][j] != '1')
-					return (0);
-			}
-		}
-	}
-	// Path validation will be added in check_path function
 	return (1);
 }
+
+// static int validate_chars(t_map *map)
+// {
+// 	int i;
+// 	int j;
+// 	i = -1;
+// 	while (++i < map->height)
+// 	{
+// 		j = -1;
+// 		while (map->grid[i][++j])
+// 		{
+// 			if (map->grid[i][j] == 'P')
+// 			{
+// 				map->player++;
+// 				map->player_x = j;
+// 				map->player_y = i;
+// 			}
+// 			else if (map->grid[i][j] == 'E')
+// 				map->exit++;
+// 			else if (map->grid[i][j] == 'C')
+// 				map->collectibles++;
+// 			else if (map->grid[i][j] != '0' && map->grid[i][j] != '1')
+// 				return (0);
+// 		}
+// 	}
+// 	return (map->player == 1 && map->exit == 1 && map->collectibles > 0);
+// }
+
+// int validate_map(t_map *map)
+// {
+// 	int i;
+// 	int j;
+// 	i = -1;
+// 	while (++i < map->height)
+// 	{
+// 		j = -1;
+// 		while (++j < map->width)
+// 		{
+// 			if (i == 0 || i == map->height - 1 || 
+// 				j == 0 || j == map->width - 1)
+// 			{
+// 				if (map->grid[i][j] != '1')
+// 					return (0);
+// 			}
+// 		}
+// 	}
+// 	return (1);
+// }
--- so_long/so_long.c
+++ so_long/so_long.c
@@ -1,32 +1,29 @@
 
 #include "so_long.h"
 
+void	free_map(char **map)
+{
+	int	i;
+
+	i = 0;
+	while (map[i])
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
+}
+
 int main(int argc, char **argv)
 {
-	char	*line;
-	int		i;
-	int		j;
-	char **map;
+	t_map	map;
 
-	int fd = open(argv[1], O_RDONLY);
-	line = get_next_line(fd);
-	while (line)
-	{
-
-		line = get_next_line(fd);
-	}
-	ft_putnbr_fd(argc, 1);
-}
-
-void	parse_map()
-{
-	char **map;
-
-	map = init_map(map);
-}
-
-
-init_map()
-{
+	if (argc != 2)
+        return (1);
+	if (parse_map(&map, argv[1]) == 0)
+		return (1);	
 	
+	free_map(map.grid);
+	return (0);		
 }
+
