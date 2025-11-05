6dcc5c817eeb30957514520c8d8be661ad49f1fc
SaadM33 <smaaqili@gmail.com>
2025-11-05 22:43:43 +0100

    done ex5

---
+++ CPP01_toKeep/ex05/Harl.cpp
@@ -0,0 +1,37 @@
+#include "Harl.hpp"
+
+void	Harl::debug( void )
+{
+	std::cout << "[ DEBUG ] I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info( void )
+{
+	std::cout << "[ INFO ] I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void	Harl::warning( void )
+{
+	std::cout << "[ WARNING ] I think I deserve to have some extra bacon for free. I've been coming here for years and you just started working here last month." << std::endl;
+}
+
+void	Harl::error( void )
+{
+	std::cout << "[ ERROR ] This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain( std::string level)
+{
+	void	(Harl::*funcs[])( void ) = { Harl::debug, Harl::info, Harl::warning, Harl::error };
+	std::string	levels[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == levels[i])
+		{
+			(this->*funcs[i])();
+			return ;
+		}
+	}
+	
+}
