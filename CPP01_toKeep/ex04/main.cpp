ef0538f2a894c4e5b5c842465f7c4240b5f31af6
SaadM33 <smaaqili@gmail.com>
2025-11-05 17:05:12 +0100

    done ex04 cp01

--- CPP01_toKeep/ex04/main.cpp
+++ CPP01_toKeep/ex04/main.cpp
@@ -1,18 +1,48 @@
-#include <fstream>
+
 #include <iostream>
-using namespace std;
+#include <fstream>
 
-int main() {
-    ifstream file("output.txt");
-    string line;
+int	main(int ac, char **av)
+{
+	if (ac != 4)
+	{
+		std::cerr << "Usage: " << av[0] << " <filename> <s1> <s2>" << std::endl;
+		return 1;
+	}
+	
+	std::ifstream	file(av[1]);
+	if (!file)
+	{
+		perror("Error");
+		return 1;
+	}
 
-    if (!file) {
-        cerr << "Cannot open file.\n";
-        return 1;
-    }
+	std::ofstream	outFile((std::string(av[1]) + ".replace").c_str());
+	if (!outFile)
+	{
+		perror("Error");
+		return 1;
+	}
 
-    while (getline(file, line))
-        cout << line ;
+	std::string line;
+	while (std::getline(file, line))
+	{
+		size_t	pos = 0;
+		while (1)
+		{
+			pos = line.find(av[2], pos);
+			if (pos == std::string::npos)
+				break ;
+			std::cout << "Found at position: " << pos << std::endl;
+			line.erase(pos, std::string(av[2]).size());
+			line.insert(pos, av[3]);
+			pos += std::string(av[3]).size();
+		}
+		outFile << line;
+		if (!file.eof())
+			outFile << std::endl;
 
-    file.close();
+	}
+
+	return 0;
 }
