15f86def98276772a8c760cdcc66b0e1627ef02f
SaadM33 <smaaqili@gmail.com>
2025-11-10 18:20:06 +0100

    Implement constructors and conversion functions in Fixed class

--- CPP02_toKeep/ex01/Fixed.cpp
+++ CPP02_toKeep/ex01/Fixed.cpp
@@ -1,6 +1,7 @@
 
 #include "Fixed.hpp"
 #include <iostream>
+#include <cmath>
 
 Fixed::Fixed( void ) : value(0)
 {
@@ -12,6 +13,17 @@ Fixed::Fixed( Fixed const &src)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	this->value = src.getRawBits();
+}
+
+Fixed::Fixed( const int value) : value(value)
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float fvalue)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->value = roundf(fvalue * (1 << this->width));
 }
 
 Fixed::~Fixed( void )
@@ -32,9 +44,26 @@ void	Fixed::setRawBits( int const raw )
 	return ;
 }
 
+float	Fixed::toFloat( void ) const
+{
+	return ((float)this->value / (1 << this->width));
+}
+
+int		Fixed::toInt( void ) const
+{
+	return (this->value >> this->width);
+}
+
 Fixed	&Fixed::operator=( Fixed const & operand )
 {
 	std::cout << "Copy assignment operator called" << std::endl;
 	this->value = operand.getRawBits();
 	return *this;
 }
+// • A member function float toFloat( void ) const;
+// that converts the fixed-point value to a floating-point value.
+// • A member function int toInt( void ) const;
+// that converts the fixed-point value to an integer value.
+// And add the following function to the Fixed class files:
+// • An overload of the insertion («) operator that inserts a floating-point representation
+// of the fixed-point number into the output stream object passed as a parameter.
