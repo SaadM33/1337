b71596887d99e385bd4fa4fca93088fc97418a02
SaadM33 <smaaqili@gmail.com>
2025-11-14 20:56:43 +0100

    remove float f

--- CPP02_toKeep/ex03/main.cpp
+++ CPP02_toKeep/ex03/main.cpp
@@ -4,14 +4,14 @@
 
 int main( void )
 {
-	Point a( 0.0f, 0.0f );
-	Point b( 10.0f, 0.0f );
-	Point c( 5.0f, 10.0f );
-	
-	Point p1( 5.0f, 5.0f );
-	Point p2( 0.0f, 0.0f );
-	Point p3( 15.0f, 15.0f );
-	
+	Point a( 0, 0 );
+	Point b( 10, 0 );
+	Point c( 5, 10 );
+
+	Point p1( 5, 5 );
+	Point p2( 0, 0 );
+	Point p3( 15, 15 );
+
 	std::cout << "Triangle: A(0,0) B(10,0) C(5,10)" << std::endl;
 	std::cout << std::endl;
 	
